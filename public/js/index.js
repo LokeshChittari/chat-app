2ec323702d6166cece952c9f6eba99b44626a97d
lokesh chittari <chittarilokesh22@gmail.com>
2018-06-12 12:27:25 +0530

    Created and emit email events from user to server and from server to user

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,20 @@
+//For cleint side-> it prints following message sin cleint side conrol i.e., browser
 var socket = io();
-
+//Listening connect->Prints following message on cleint side if it is connected to server
 socket.on('connect', function () {
   console.log('Connected to server');
 });
-
+//Listening disconnect->Prints following message on cleint side if it is disconnected to server
 socket.on('disconnect', function () {
   console.log('user disconnected from server');
+});
+//EMitting event i.e., createEmail from cleint side
+socket.emit('createEmail', {
+  to:'jen@example.com',
+  text:'Hey. This is Lokesh'
+});
+
+// Listening event i.e., newEmail ->Following message prints on cleint side when newEmail is emitted.
+socket.on('newEmail', function (email) {
+  console.log('New Email',email);
 });
--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ var io = socketIO(server);
 //Setting listener for the socket-> it displays message given when user connected
 io.on('connection', (socket) => {
   console.log('New User Connected');
+//Emitting the event i.e., newEmail event from server side, sending objects
+socket.emit('newEmail', {
+  from:'mike@example.com',
+  text:'Hey. What is going on.',
+  createdAt:123
+});
+//Adding Listener event i.e.,Creating email on server side following message prints on server side when emitted this event
+socket.on('createEmail', (newEmail) => {
+  console.log('createEmail', newEmail);
+});
 //It displays following message if user disconnected
   socket.on('disconnect', ()=>{
     console.log('User disconnected');
